95a4f4b71014d7fd2e566e99b6bc9905c4e43475
Jubair Alam Ovi <jubair.ovi@northsouth.edu>
2022-04-16 14:30:05 +0600

    ElevatedButton

--- flutter_mad_git_1/lib/main.dart
+++ flutter_mad_git_1/lib/main.dart
@@ -1,14 +1,46 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+//import 'package:flutter/cupertino.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(primarySwatch: Colors.cyan),
+    theme: ThemeData(
+      primarySwatch: Colors.cyan,
+    ),
     home: Scaffold(
       appBar: AppBar(
-        title: Text('By the name of Allah'),
+        title: const Text('By the name of Allah '),
+      ),
+      body: Center(
+        child: Column(
+          //mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'By the name of Allah ',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 30),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  child: Text('Submit'),
+                  onPressed: () {},
+                ),
+                SizedBox(
+                  width: 19,
+                ),
+                ElevatedButton(
+                  child: Text('Cancel2'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     ),
   ));
